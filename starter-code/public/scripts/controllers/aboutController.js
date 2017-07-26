7e34d960646053f75257ff52efcec28470cdf394
Rami Bououni <rami.bououni@gmail.com>
2017-07-25 17:40:14 -0700

    finished calling a function to load the data

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -7,8 +7,11 @@ var app = app || {};
   aboutController.index = () => {
     $('#about').show().siblings().hide(); // REVIEW: We have a slight refactor in selectors here, which has reduced the amount of code from the last lab.
 
-    // TODO: Call a function to load all the data.
+    // DONE: Call a function to load all the data.
     // Pass a view function as a callback, so the view will render after the data is loaded.
+    // estimate 10 min, actual 5 min
+
+    app.repos.requestRepos(app.repoView.index);
 
   };
 
